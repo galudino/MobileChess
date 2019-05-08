6ba826892109801f4518b241f611024c48fbfa17
Gem Aludino <g.aludino@gmail.com>
2019-05-08 19:33:34 -0400

    Revert "Starting to implement Playback."
    This reverts commit b04fa758

--- app/src/main/java/com/rutgers/chess22/ChessActivity.java
+++ app/src/main/java/com/rutgers/chess22/ChessActivity.java
@@ -123,7 +123,7 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
 
     private boolean resignRequested;    // activated by buttonResign
 
-    Game game;
+    private Game game;
 
     private PieceType promotionType;
 
--- app/src/main/java/com/rutgers/chess22/PlaybackGame.java
+++ app/src/main/java/com/rutgers/chess22/PlaybackGame.java
@@ -3,31 +3,13 @@ package com.rutgers.chess22;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class PlaybackGame extends ChessActivity implements View.OnClickListener {
-
-    private TextView playersTurn;
-
-    private Button btnBackward;
-    private Button btnForward;
-    private Button btnMainMenu;
+public class PlaybackGame extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playback_game);
-
-        playersTurn = findViewById(R.id.playersTurn);
-        playersTurn.setText(game.isWhitesMove() ? "Whites turn" : "Blacks turn");
-
-        btnBackward = findViewById(R.id.btnBackward);
-        btnForward = findViewById(R.id.btnForward);
-        btnMainMenu = findViewById(R.id.btnMainMenu);
-
-
-        initializeChessboard(this);
     }
 
     @Override
